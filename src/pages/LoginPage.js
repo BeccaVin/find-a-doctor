2a8b29c0ad32ebff53f833a9d5195e4415bc1969
Rebecca Vinet <rebecca.vinet@outlook.com>
2024-04-15 00:46:06 -0400

    styling for login page

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import HeaderLogin from '../components/Header/HeaderLogin';
+import Login from '../components/Login/Login';
+function LoginPage() {
+    return (
+        <>
+            <HeaderLogin />
+            <Login />
+        </>
+
+    );
+  }
+  
+  export default LoginPage;
