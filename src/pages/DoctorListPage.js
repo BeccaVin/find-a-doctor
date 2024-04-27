699aa68da4f5ae2b9066df46e5e4d69a4c51d7ad
Rebecca Vinet <rebecca.vinet@outlook.com>
2024-04-27 15:36:51 -0400

    added footer component and doctor list page

--- src/pages/DoctorListPage.js
+++ src/pages/DoctorListPage.js
@@ -1,10 +1,12 @@
 import Header from '../components/Header/Header';
 import DoctorList from '../components/DoctorList/DoctorList';
+import Footer from '../components/Footer/Footer';
 function DoctorListPage() {
     return (
         <>
             <Header />
             <DoctorList />
+            <Footer />
         </>
 
     );
